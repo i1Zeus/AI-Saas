4bf81f8bdb5c45bedb27916b4c05e045a6c26e3e
Hussein Najah <husseinnajah123@gmail.com>
2023-07-21 01:18:44 +0300

    logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
       <div className="px-3 py-2 flex-1">
         <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
-            <Image fill alt="logo" src="/sideBarLogo.png" />
+            <Image fill alt="logo" src="/logo.png" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             QuantaGen
