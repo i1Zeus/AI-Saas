8aad034e3d2dfa6e67350171c824c5b65f051fd3
Hussein Najah <husseinnajah123@gmail.com>
2023-07-21 01:49:11 +0300

    sheet - mobile side bar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,12 +3,21 @@
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
 
 const MobileSidebar = () => {
   return (
-    <Button size="icon" variant="ghost" className="md:hidden">
-      <Menu className="w-6 h-6" />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button size="icon" variant="ghost" className="md:hidden">
+          <Menu className="w-6 h-6" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 };
 
