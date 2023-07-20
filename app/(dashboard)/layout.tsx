6c18e0874ba13bbacd757e58852b422690a8d990
Hussein Najah <husseinnajah123@gmail.com>
2023-07-20 20:59:11 +0300

    dash layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className="hidden bg-gray-900 h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 md:z-[80] ">
+        <div>Side Bar</div>
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
