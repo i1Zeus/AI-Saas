7d64ea5d6cdcf4846e1b58405c701b32c50a3ed7
Hussein Najah <husseinnajah123@gmail.com>
2023-07-20 20:09:22 +0300

    stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI-Sass",
-  description: "Ai-Sass",
+  title: "QuantaGen",
+  description: "AI Platform for the Future",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className=""> Hello AI-Saas </p>
+    <>
+      
+    </>
   );
 }
