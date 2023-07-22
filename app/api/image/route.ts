0d0c822c50e03b11140d9ba69150ba96759797e4
Hussein Najah <husseinnajah123@gmail.com>
2023-07-22 16:57:19 +0300

    APIT route

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,9 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 
+// import { checkSubscription } from "@/lib/subscription";
+// import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -14,25 +17,44 @@ export async function POST(req: Request) {
     const body = await req.json();
     const { prompt, amount = 1, resolution = "512x512" } = body;
 
-    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-    if (!configuration.apiKey)
+    if (!configuration.apiKey) {
       return new NextResponse("OpenAI API Key not configured.", {
         status: 500,
       });
+    }
 
-    if (!prompt) return new NextResponse("Prompt is required", { status: 400 });
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
 
-    if (!amount) return new NextResponse("Amount is required", { status: 400 });
+    if (!amount) {
+      return new NextResponse("Amount is required", { status: 400 });
+    }
 
-    if (!resolution)
+    if (!resolution) {
       return new NextResponse("Resolution is required", { status: 400 });
+    }
+
+    // const freeTrial = await checkApiLimit();
+    // const isPro = await checkSubscription();
+
+    // if (!freeTrial && !isPro)
+    //   return new NextResponse(
+    //     "Free trial has expired. Please upgrade to pro.",
+    //     { status: 403 }
+    //   );
 
     const response = await openai.createImage({
       prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+
+    // if (!isPro) await incrementApiLimit();
 
     return NextResponse.json(response.data.data);
   } catch (error) {
