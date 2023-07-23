7c45fd526ca3c2ebbae8cd1a78d312dbd005c3f3
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 23:01:50 +0300

    nuxt theme

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("e1a31636-4b52-4484-8a8b-c89d4c573e56");
+    Crisp.configure("2308d92a-a90c-416e-95b2-f7552766ce51");
   }, []);
 
   return null;
