fa9b5746521d59ec0e7ae95bd6809dce91fe9f84
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 16:19:10 +0300

    constants + api limit

---
+++ lib/api-limit.ts
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/prismadb";
+import { MAX_FREE_COUNT } from "@/constants";
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+  if (!userId) return;
+
+  const userAPILimit = await prismadb.userApiLimit.findUnique({
+    where: { userId },
+  });
+
+  if (userAPILimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId },
+      data: { count: userAPILimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: { userId, count: 1 },
+    });
+  }
+};
+
+export const isApiLimitReached = async () => {
+  const { userId } = auth();
+  if (!userId) return false;
+
+  const userAPILimit = await prismadb.userApiLimit.findUnique({
+    where: { userId },
+  });
+
+  if (!userAPILimit || userAPILimit.count < MAX_FREE_COUNT) {
+    return true;
+  } else {
+    return false;
+  }
+};
