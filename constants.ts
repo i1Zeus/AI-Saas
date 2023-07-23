a5d90feb887eab4d31d1ecd904ad65c401d26470
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 22:38:26 +0300

    chat + constans

--- constants.ts
+++ constants.ts
@@ -1,1 +1,41 @@
+import { Code, ImageIcon, MessageSquare, Music, Video } from "lucide-react";
+
 export const MAX_FREE_COUNT = 5;
+
+export const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/video",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+];
