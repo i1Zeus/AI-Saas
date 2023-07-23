131405292d820d07a3feb7997a17ea53f095fd9d
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 22:37:38 +0300

    modals

--- components/modal-provider.tsx
+++ components/modal-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ProModal from "@/components/pro-modal";
+import { ProModal } from "@/components/pro-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,86 +1,63 @@
-import { useProModal } from "@/hooks/use-pro-modal";
-import {
-  Check,
-  Code,
-  ImageIcon,
-  MessageSquare,
-  Music,
-  Video,
-  Zap,
-} from "lucide-react";
-import { cn } from "@/lib/utils";
+"use client";
 
-import { Card } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useState } from "react";
+import { Check, Zap } from "lucide-react";
+import { toast } from "react-hot-toast";
+
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
+  DialogFooter,
 } from "@/components/ui/dialog";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { tools } from "@/constants";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
-const tools = [
-  {
-    label: "Conversation",
-    icon: MessageSquare,
-    color: "text-violet-500",
-    bgColor: "bg-violet-500/10",
-  },
-  {
-    label: "Image Generation",
-    icon: ImageIcon,
-    color: "text-pink-500",
-    bgColor: "bg-pink-500/10",
-  },
-  {
-    label: "Video Generation",
-    icon: Video,
-    color: "text-orange-500",
-    bgColor: "bg-orange-500/10",
-  },
-  {
-    label: "Music Generation",
-    icon: Music,
-    color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10",
-  },
-  {
-    label: "Code Generation",
-    icon: Code,
-    color: "text-green-700",
-    bgColor: "bg-green-700/10",
-  },
-];
-
-const ProModal = () => {
+export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-            <div className="flex items-center gap-x-2 font-bold py-1">
-              Upgrade to QuantaGen
+            <div className="flex items-center gap-x-2 font-bold text-xl">
+              Upgrade to Genius
               <Badge variant="premium" className="uppercase text-sm py-1">
-                Pro
+                pro
               </Badge>
             </div>
           </DialogTitle>
-          <DialogDescription className="text-center pt-2 space-y-2 font-medium text-zinc-900">
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
             {tools.map((tool) => (
               <Card
-                key={tool.label}
+                key={tool.href}
                 className="p-3 border-black/5 flex items-center justify-between"
               >
                 <div className="flex items-center gap-x-4">
                   <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                    <tool.icon
-                      className={cn("w-6 h-6", tool.color)}
-                      size={24}
-                    />
+                    <tool.icon className={cn("w-6 h-6", tool.color)} />
                   </div>
                   <div className="font-semibold text-sm">{tool.label}</div>
                 </div>
@@ -90,7 +67,13 @@ const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            disabled={loading}
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
@@ -99,5 +82,3 @@ const ProModal = () => {
     </Dialog>
   );
 };
-
-export default ProModal;
