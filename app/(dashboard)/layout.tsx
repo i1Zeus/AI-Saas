4949d1913a2e59e3e2f25a184660f8febb73bd77
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 17:49:15 +0300

    stuff

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full relative">
-      <div className="hidden bg-gray-900 h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 md:z-[80] ">
+      <div className="hidden bg-gray-900 h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
