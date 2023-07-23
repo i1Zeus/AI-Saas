01581e01b8a6938fb0caf478d87c51a1878ddc9e
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 22:33:14 +0300

    layotut

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,30 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "./globals.css";
+import { ToasterProvider } from "@/components/toaster-provider";
 import { ModalProvider } from "@/components/modal-provider";
+import { CrispProvider } from "@/components/crisp-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "QuantaGen",
-  description: "AI Platform for the Future",
+  description: "AI Platform",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
+      <html lang="en" suppressHydrationWarning>
+        <CrispProvider />
+        <body className={font.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
