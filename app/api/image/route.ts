cb74f8b360c7976d9bee5856a60a361456561451
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 15:32:09 +0300

    image api

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -17,27 +17,19 @@ export async function POST(req: Request) {
     const body = await req.json();
     const { prompt, amount = 1, resolution = "512x512" } = body;
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
 
-    if (!configuration.apiKey) {
+    if (!configuration.apiKey)
       return new NextResponse("OpenAI API Key not configured.", {
         status: 500,
       });
-    }
 
-    if (!prompt) {
-      return new NextResponse("Prompt is required", { status: 400 });
-    }
+    if (!prompt) return new NextResponse("Prompt is required", { status: 400 });
 
-    if (!amount) {
-      return new NextResponse("Amount is required", { status: 400 });
-    }
+    if (!amount) return new NextResponse("Amount is required", { status: 400 });
 
-    if (!resolution) {
+    if (!resolution)
       return new NextResponse("Resolution is required", { status: 400 });
-    }
 
     // const freeTrial = await checkApiLimit();
     // const isPro = await checkSubscription();
