de4ecb483a4ae7485bce12caced22327050ccca7
Hussein Najah <husseinnajah123@gmail.com>
2023-07-23 16:38:35 +0300

    prisma

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,4 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismadb = global.prisma || new PrismaClient();
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
