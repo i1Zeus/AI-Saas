95ae6a4ed380f2db73170b69257186499df576cb
Hussein Najah <husseinnajah123@gmail.com>
2023-07-26 01:28:37 +0300

    tool item

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { tools } from "@/constants";
 
-import { Card } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { ArrowRight } from "lucide-react";
 import { ToolItem } from "@/components/tool-item";
 
 const DashboardPage = () => {
@@ -23,24 +19,7 @@ const DashboardPage = () => {
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {/* {tools.map((tool) => (
-          <Card
-            onClick={() => router.push(tool.href)}
-            key={tool.href}
-            className="p-4 border-black/54 flex items-center justify-between hover:drop-shadow-[-3px_3px_1px_rgb(168_178_188/_0.3)] transition cursor-pointer group"
-          >
-            <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-6 h-6", tool.color)} />
-              </div>
-              <div className="font-semibold">
-                {tool.label.charAt(0).toUpperCase() + tool.label.slice(1)}
-              </div>
-            </div>
-            <ArrowRight className="w-6 h-6 text-black/54 mr-2 group-hover:mr-0" />
-          </Card>
-        ))} */}
-        <ToolItem />
+        <ToolItem modal={false} />
       </div>
     </div>
   );
