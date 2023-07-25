d57db75364a5b3dd56bb484511b39197685ff4a0
Hussein Najah <husseinnajah123@gmail.com>
2023-07-25 18:47:30 +0300

    hero

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,14 +12,16 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          The Best AI Tool for
+        </h1>
+        <div className="text-white">
           <TypewriterComponent
             options={{
               strings: [
                 "Chatbot.",
                 "Photo Generation.",
-                "Blog Writing.",
+                "Code Generation.",
                 "Mail Writing.",
               ],
               autoStart: true,
