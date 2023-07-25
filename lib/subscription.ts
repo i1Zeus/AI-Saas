294b81d089721dc734b64ad4d5a1305c9647fff4
Hussein Najah <husseinnajah123@gmail.com>
2023-07-25 19:45:09 +0300

    sub

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -7,9 +7,7 @@ const DAY_IN_MS = 86_400_000;
 export const checkSubscription = async () => {
   const { userId } = auth();
 
-  if (!userId) {
-    return false;
-  }
+  if (!userId) return false;
 
   const userSubscription = await prismadb.userSubscription.findUnique({
     where: {
